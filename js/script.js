d2d0f4f1693cbf9fdff567ed03dc792893849350
Nazrul144 <nazrulislam.cse28@gmail.com>
2024-03-03 15:50:16 +0600

    dynamic part done for authore section

--- js/script.js
+++ js/script.js
@@ -4,47 +4,69 @@ const loadData = async () =>{
     const response = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts')
     const data = await response.json();
     const posts = data.posts;
-    showingPosts(posts)
+    showingPosts(posts);
 }
 
 const showingPosts = (posts) =>{
     
     const postsContainer = document.getElementById('card-container');
+    //Active symbol adding:
+
+   
+
     posts.forEach((post)=>{
         console.log(post);
 
         //Create a div element:
         const div = document.createElement('div');
-        div.classList = `card lg:w-7/12 bg-base-100 shadow-xl py-8`;
+        div.classList = `card bg-[#F3F3F5] shadow-xl py-8 `;
         div.innerHTML = `
+        <div class = "h-4 w-4 rounded-full absolute ml-28 mt-6 bg-green-400">
+        </div>
         <div class="flex">
         <div class="ml-8 mt-8 ">
-            <figure><img class = "w-20 h-20 rounded-lg" src="${post.image}" alt="Shoes" /></figure>
+            <figure><img class = "w-24 h-24 rounded-lg" src="${post.image}" alt="Shoes" /></figure>
         </div>
         <div class="card-body ">
-            <h2 class="card-title">Music <span>Author: Nazrul Islam</span></h2>
-            <p>10 Kids Unaware of Their Halloween Costume</p>
-            <p>10 Kids Unaware of Their Halloween Costume</p>
+            <h2 class="card-title">#<span>${post.category}<span class ="ml-4">Author:</span> ${post.author.name}</span></h2>
+            <p class = "text-xl font-bold">${post.title}</p>
+            <p>${post.description}</p>
             <hr>
             <div class="flex justify-evenly gap-28">
               <div class="flex gap-8">
-                <div><i class="fa-regular fa-message"></i></div>
-              <div><i class="fa-regular fa-eye"></i></div>
-              <div><i class="fa-regular fa-clock"></i></div>
+                <div><i class="fa-regular fa-message"></i> <span>${post.comment_count}</span></div>
+              <div><i class="fa-regular fa-eye"></i><span></span> ${post.view_count}</div>
+              <div><i class="fa-regular fa-clock"></i><span> ${post.posted_time} min</span></div>
               </div>
               <div>
-                <i class="fa-regular fa-envelope-open"></i>
+                <button onclick ="addingAnotherInfo('${post.title}','${post.view_count}')" class = "bg-[#10B981] w-10 h-10  rounded-full"><i class="fa-regular fa-envelope-open"></i></button>
             </div>
             </div>
            
           </div>
     </div> 
   </div>
-        
         `;
         postsContainer.appendChild(div);
 
     })
+
 }
+
+const addingAnotherInfo = (title,view_count) =>{
+  console.log(title, view_count);
+  const addingAuthorInfo = document.getElementById('addingAuthorInfo');
+
+  const div = document.createElement('div');
+  div.classList = `bg-white rounded-lg flex justify-between px-4 py-2 shadow-2xl mt-4 font-bold`
+  div.innerHTML = `
+  <h1>${title}</h1>
+  <p><i class="fa-regular fa-eye"></i><span>${view_count}</span></p>
+  `;
+  addingAuthorInfo.appendChild(div);
+
+}
+
+
 
 loadData();
