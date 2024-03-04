b0e796301636211470f7105e9bb96f822490bbb3
Nazrul144 <nazrulislam.cse28@gmail.com>
2024-03-04 16:30:39 +0600

    project updated

--- js/script.js
+++ js/script.js
@@ -1,30 +1,25 @@
-console.log("Connected!");
 
-const loadData = async (inputText) =>{
-    const response = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${inputText}`)
-    const data = await response.json();
-    const posts = data.posts;
-    showingPosts(posts);
+const loadData = async (inputText) => {
+  const response = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${inputText}`)
+  const data = await response.json();
+  const posts = data.posts;
+  showingPosts(posts);
 }
 
 
+const showingPosts = (posts) => {
 
+  const postsContainer = document.getElementById('card-container');
+  //Clear card-container before adding new card:
+  postsContainer.textContent = '';
 
+  posts.forEach((post) => {
+    console.log(post);
 
-
-const showingPosts = (posts) =>{
-    
-    const postsContainer = document.getElementById('card-container');
-    //Clear card-container before adding new card:
-    postsContainer.textContent = '';
-
-    posts.forEach((post)=>{
-        console.log(post);
-
-        //Create a div element:
-        const div = document.createElement('div');
-        div.classList = `card bg-[#F3F3F5] shadow-xl py-8 `;
-        div.innerHTML = `
+    //Create a div element:
+    const div = document.createElement('div');
+    div.classList = `card bg-[#F3F3F5] shadow-xl py-8 `;
+    div.innerHTML = `
         <div class = "h-4 w-4 rounded-full absolute ml-28 mt-6 ${post?.isActive ? 'bg-green-400' : 'bg-red-400'}">
         
         </div>
@@ -36,7 +31,7 @@ const showingPosts = (posts) =>{
             <h2 class="card-title">#<span>${post.category}<span class ="ml-4">Author:</span> ${post.author.name}</span></h2>
             <p class = "text-xl font-bold">${post.title}</p>
             <p>${post.description}</p>
-            <hr>
+            <hr style="border-top: dashed 2px;">
             <div class="flex justify-evenly gap-28">
               <div class="flex gap-8">
                 <div><i class="fa-regular fa-message"></i> <span>${post.comment_count}</span></div>
@@ -52,30 +47,23 @@ const showingPosts = (posts) =>{
     </div> 
   </div>
         `;
-        postsContainer.appendChild(div);
+    postsContainer.appendChild(div);
 
-        // if(post.isActive){
-        //   div.style.backgroundColor = 'green'
-        // }else{
-        //   div.style.backgroundColor = 'red'
-        // }
-    })
+  })
 
-  
-    
+
+
 }
 
-
-
 let sum = 1;
-const addingAnotherInfo = (title,view_count) =>{
+const addingAnotherInfo = (title, view_count) => {
 
   //Card-Counting: 
   const currentCard = document.getElementById('countCard');
-  const convertToNumber = +currentCard.innerText; 
-  let countingTotalCard = parseInt(convertToNumber) + sum;
+  const convertToNumber = +currentCard.innerText;
+  let countingTotalCard = convertToNumber + sum;
   console.log(countingTotalCard);
-  currentCard.innerHTML = countingTotalCard; 
+  currentCard.innerHTML = countingTotalCard;
 
 
   console.log(title, view_count);
@@ -92,27 +80,27 @@ const addingAnotherInfo = (title,view_count) =>{
 }
 
 //Latest Post: 
-const latestPost = async() =>{
+const latestPost = async () => {
   const response = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts')
   const latestPosts = await response.json();
   displayLatestPosts(latestPosts);
-  
+
 }
 
-const displayLatestPosts = (latestPosts) =>{
+const displayLatestPosts = (latestPosts) => {
   const latestPostContainer = document.getElementById('latestPostContainer');
 
-  latestPosts.forEach((latestPost) =>{
+  latestPosts.forEach((latestPost) => {
     console.log(latestPost);
-       const div = document.createElement('div');
-       div.classList = ` card card-compact w-96 bg-base-100 shadow-xl `
+    const div = document.createElement('div');
+    div.classList = ` card card-compact w-full bg-base-100 shadow-xl `
 
 
-       div.innerHTML = ` 
-       <div class="card card-compact w-96 h-full bg-base-100 shadow-xl">
+    div.innerHTML = ` 
+       <div class="card card-compact w-full h-full bg-base-100 shadow-xl">
                     <figure><img src="${latestPost.cover_image}" alt="Shoes" /></figure>
                     <div class="card-body">
-                      <h2 class="card-title"><i class="fa-regular fa-calendar"></i><span>${latestPost.author?.posted_date? latestPost.author.posted_date:'No publish date'}</span></h2>
+                      <h2 class="card-title"><i class="fa-regular fa-calendar"></i><span>${latestPost.author?.posted_date ? latestPost.author.posted_date : 'No publish date'}</span></h2>
                       <p class = "font-bold text-xl">${latestPost.title}</p>
                       <p>${latestPost.description}</p>
                       <div class="flex gap-2 items-center">
@@ -127,24 +115,35 @@ const displayLatestPosts = (latestPosts) =>{
                     </div>
                   </div>
        `;
-       latestPostContainer.appendChild(div);
+    latestPostContainer.appendChild(div);
   })
 }
 
 
 
 //Search Function:
-const inputSearchField = () =>{
+const inputSearchField = () => {
+  //Validity check:
+
   //Spinner
   const spinner = document.getElementById('spinner');
   spinner.classList.remove('hidden');
   setTimeout(() => {
     spinner.classList.add('hidden');
-  },2000);
+  }, 2000);
+
+
 
   const inputSearchField = document.getElementById('inputSearchField');
   const inputText = inputSearchField.value;
-  loadData(inputText);
+
+  if (inputText === "") {
+    alert("Input box can't be empty!");
+  } else {
+    loadData(inputText);
+  }
+
+
 }
 
 latestPost();
